935e84604786dc26f9135f20a8bade8da181f7c6
sukhrobdev <suxrobdevbk@gmail.com>
2022-07-10 15:50:29 +0500

    Done!

--- src/main/java/uz/sudev/clickapp/payload/Message.java
+++ src/main/java/uz/sudev/clickapp/payload/Message.java
@@ -15,7 +15,6 @@ public class Message {
     private boolean success;
     private String message;
     private String token;
-
     public Message(boolean success, String message) {
         this.success = success;
         this.message = message;
